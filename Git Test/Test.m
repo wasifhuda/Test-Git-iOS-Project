642780d20976aaaacac566ad0494d026d5adcb73
Wasif <wasif.huda@gmail.com>
2015-02-11 21:45:33 -0600

    added a test class

---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Wasif on 2/11/15.
+//  Copyright (c) 2015 IhsaanFusion. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
